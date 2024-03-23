933dec66355b0b16b397d4bf674c58d07eb56228
Duvanov A.A. <ar.duwanov007@gmail.com>
2024-03-23 12:14:37 +0300

    add db for user group saving

--- src/bot.py
+++ src/bot.py
@@ -1,18 +1,63 @@
 import datetime
 import logging
 import os
+import sqlite3
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes, MessageHandler, filters
 
 import parser
 
+
+# Функция для создания таблицы пользователей в базе данных SQLite
+def create_user_table():
+    conn = sqlite3.connect('user_data.db')
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS users
+                      (user_id INTEGER PRIMARY KEY, group_name TEXT)''')
+    conn.commit()
+    conn.close()
+
+
+# Функция для добавления информации о пользователе в базу данных SQLite
+def add_user_data(user_id, group_name):
+    conn = sqlite3.connect('user_data.db')
+    cursor = conn.cursor()
+
+    # Проверяем, есть ли пользователь уже в базе данных
+    cursor.execute("SELECT COUNT(*) FROM users WHERE user_id=?", (user_id,))
+    count = cursor.fetchone()[0]
+
+    if count > 0:
+        # Если пользователь уже есть в базе данных, обновляем его группу
+        cursor.execute("UPDATE users SET group_name=? WHERE user_id=?", (group_name, user_id))
+    else:
+        # Если пользователь отсутствует в базе данных, добавляем его
+        cursor.execute("INSERT INTO users (user_id, group_name) VALUES (?, ?)", (user_id, group_name))
+
+    conn.commit()
+    conn.close()
+
+
+
+# Функция для получения информации о пользователе из базы данных SQLite
+def get_user_data(user_id):
+    conn = sqlite3.connect('user_data.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT group_name FROM users WHERE user_id=?", (user_id,))
+    row = cursor.fetchone()
+    conn.close()
+    if row:
+        return row[0]
+    else:
+        return None
+
+
 # Состояние смены типа недели (по умолчанию числитель)
 week_type = "Числитель"
 
 back_key = InlineKeyboardMarkup([[InlineKeyboardButton("Назад", callback_data="change_week_type")]])
 
-group = None
 
 # Enable logging
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
@@ -42,7 +87,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Parses the CallbackQuery and updates the message text."""
     global week_type  # Делаем week_type глобальной переменной
-    global group
 
     query = update.callback_query
     await query.answer()
@@ -63,7 +107,9 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     elif query.data == "settings":
         await query.edit_message_text(text="Введите название группы:", reply_markup=back_key)
     elif query.data.isnumeric():
-        mes = parser.main(group_name=group, day=int(query.data), week_type=week_type)
+        user_id = query.from_user.id
+        bd_group = get_user_data(user_id)
+        mes = parser.main(group_name=bd_group, day=int(query.data), week_type=week_type)
         await query.edit_message_text(mes, reply_markup=back_key)
     else:
         today_wd = datetime.datetime.now().weekday()
@@ -101,7 +147,14 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         if shift_day == 6:
             mes = "По воскресеньям не учимся"
         else:
-            mes = parser.main(group_name=group, day=shift_day, switch_wt=sw)
+            # При необходимости, получите информацию о пользователе из базы данных
+            user_id = query.from_user.id
+            group = get_user_data(user_id)
+
+            if group is not None:
+                mes = parser.main(group_name=group, day=shift_day, switch_wt=sw)
+            else:
+                mes = "Сначала задайте название группы"
         try:
             await query.edit_message_text(mes, reply_markup=back_key)
         except Exception as e:
@@ -109,18 +162,17 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    global group
+    group_msg = update.message.text
 
-    group = update.message.text
-
-    answ = parser.main(group_name=group)
+    answ = parser.main(group_name=group_msg)
     if answ == -1:
         mes = "Не удалось проверить группу, попробуйте позже"
-        group = None
     elif answ == 0:
-        mes = "Не удалось найти группу " + group
-        group = None
+        mes = "Не удалось найти группу " + group_msg
     else:
+        user_id = update.message.from_user.id
+        group = update.message.text
+        add_user_data(user_id, group)
         mes = "Группа успешно сохранена: " + group
     await update.message.reply_text(text=mes, reply_markup=back_key)
 
@@ -131,6 +183,9 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 
 
 def main() -> None:
+    # Перед использованием базы данных, убедитесь, что таблица создана
+    create_user_table()
+
     """Run the bot."""
     # Create the Application and pass it your bot's token.
     application = Application.builder().token(TOKEN).build()
