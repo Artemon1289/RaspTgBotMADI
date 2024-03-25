e563d79c8554b199230e93c50c55485646b34772
Duvanov A.A. <ar.duwanov007@gmail.com>
2024-03-25 22:52:55 +0300

    add .txt logger

--- src/bot_func.py
+++ src/bot_func.py
@@ -30,18 +30,24 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             await query.edit_message_reply_markup(reply_markup=get_menu_keyboard())
         except Exception as e:
             print(e)
+
     elif query.data == "set_cur_week_type":
         new_week_type = parser.get_week_type(switch=False)
         if week_type != new_week_type:
             week_type = new_week_type
             await query.edit_message_reply_markup(get_menu_keyboard())
+
     elif query.data == "settings":
         await query.edit_message_text(text="Введите название группы:", reply_markup=back_key)
+
     elif query.data.isnumeric():
+        day_info = parser.get_weekday(int(query.data))
         user_id = query.from_user.id
-        bd_group = database.get_user_data(user_id)
-        mes = parser.main(group_name=bd_group, day=int(query.data), week_type=week_type)
+        group = database.get_user_data(user_id)
+        mes = parser.main(group_name=group, day=int(query.data), week_type=week_type)
         await query.edit_message_text(mes, reply_markup=back_key)
+        await log(id=user_id, group=group, function=day_info)
+
     else:
         today_wd = datetime.datetime.now().weekday()
         if query.data == "ПЗВЧР":
@@ -83,6 +89,7 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
             if group is not None:
                 mes = parser.main(group_name=group, day=shift_day, switch_wt=sw)
+                await log(user_id, group, query.data)
             else:
                 mes = "Сначала задайте название группы"
         try:
@@ -140,3 +147,8 @@ def get_menu_keyboard():
         [InlineKeyboardButton("Настройка группы", callback_data="settings"), ],
     ])
     return menu_keyboard
+
+
+async def log(id, group, function):
+    with open('groups_log.txt', 'a') as f:
+        f.write(f'{id} - {group} - {function} - {datetime.datetime.now()}\n')
